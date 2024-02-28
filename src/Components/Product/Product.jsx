9d1a95d2ea207ef07714cc76d6bde3049e867351
eshetu <assefaeshetu1221@gmail.com>
2024-02-28 12:34:17 +0300

    little modification on prouct jsx

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProductCard from "./ProductCard";
-import classes from "./Product.module.css";
+import classes from "./product.module.css";
+
 import Loader from "../Loader/Loader";
 function Product() {
   const [products, setProducts] = useState([]);
