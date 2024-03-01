cd69cc91167e7a99771969575eb2cee36c3d0e3f
eshetu <assefaeshetu1221@gmail.com>
2024-03-01 15:37:36 +0300

    modicifation on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+module.exports = {
+  // other configurations...
+  build: {
+    // other build options...
+    chunkSizeWarningLimit: 1000000, // <- Increased limit to 1 MB
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
